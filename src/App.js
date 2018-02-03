7f674206bb086dd8f6c117f26fad51835b99b488
gerbus <gerbus3@gmail.com>
2018-02-03 12:10:24 -0800

    Update UI, improve SEO

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import moment from 'moment';
 import queryString from 'query-string';
+import waiting from './spiffygif_40x40.gif';
 
 class App extends Component {
   constructor(props) {
@@ -76,21 +77,13 @@ class App extends Component {
             <div className="col-sm-10 col-md-8">
               
               <div className="text-back intro">
-                <h1>Last Stand: Dates</h1>
-                <p>Listed are dates within <b>{this.state.days} days</b> (today to <span className="moment date">{endDate.format("Y-M-D")}</span>) on which low tides of less than <b>{this.state.depth}m</b> occur between the hours of <b>{this.state.startHour}:00 and {this.state.endHour}:00</b> on Vancouver shores.</p>
-                
-                <div className="alert alert-info info"><b>To alter the parameters of the results</b>, use the link below and modify the querystring parameters:
-                  <table className="minimal"><tbody>
-                    <tr><td><i>startHour</i>, <i>endHour</i>&nbsp;&nbsp;</td><td>only show results between the hours of <i>startHour</i> and <i>endHour</i>; integer / hour on 24-hour clock</td></tr>
-                    <tr><td><i>days</i></td><td>only show results before today + <i>days</i>; integer</td></tr>
-                    <tr><td><i>depth</i></td><td>only show results where low-tide is less than <i>depth</i>; decimal / meters</td></tr>
-                  </tbody></table>
-                  i.e. <a href={"http://gerbus.ca/laststand?startHour=" + this.state.startHour + "&endHour=" + this.state.endHour + "&days=" + this.state.days + "&depth=" + this.state.depth}>http://gerbus.ca/laststand?startHour={this.state.startHour}&endHour={this.state.endHour}&days={this.state.days}&depth={this.state.depth}</a>
-                </div>
+                <h1>Low Tide Finder (Vancouver)</h1>
+                <p>Listed are dates within <b>{this.state.days} days</b> (today to {endDate.format("MMM Do")}) on which low tides of less than <b>{this.state.depth}m</b> occur between the hours of <b>{this.state.startHour}:00 and {this.state.endHour}:00</b> on Vancouver shores. To change search parameters, see <a href="#params">bottom of page</a>.</p>
               </div>				
 
+      
               <table className="table headroom"><thead>
-                <tr><th>When</th><th>Low Tide Height</th></tr>
+                <tr className="text-back"><th>When</th><th>Low Tide Level</th></tr>
                 </thead><tbody>
                 
                 {
@@ -99,20 +92,33 @@ class App extends Component {
                       this.state.data.map((item, index) => (
                         <tr key={index} className={'text-back ' + item.className}>
                           <td>{item.dateTime}</td>
-                          <td>{item.tideLevel}</td>
+                          <td>{item.tideLevel} m</td>
                         </tr>
                       )) 
                     ) : (
                       <tr className="text-back"><td colSpan="2">No data</td></tr>
                     )
                   ) : (
-                    <tr className="text-back"><td colSpan="2">Fetching...</td></tr>
+                    <tr className="text-back"><td colSpan="2"><center><img src={waiting} /></center></td></tr>
                   )
                 }
                   
                 </tbody>
               </table>
               
+
+              <div id="params" className="alert alert-info info"><b>To alter the parameters of the results</b>, use the link below and modify these querystring parameters as needed:
+                <table className="minimal">
+                  <tbody>
+                    <tr><td><i>days</i></td><td>only show results before today + <i>days</i>; integer</td></tr>
+                    <tr><td><i>depth</i></td><td>only show results where low-tide is less than <i>depth</i>; decimal / meters</td></tr>
+                    <tr><td><i>startHour</i>, <i>endHour</i>&nbsp;&nbsp;</td><td>only show results between the hours of <i>startHour</i> and <i>endHour</i>; integer / hour on 24-hour clock</td></tr>
+
+                  </tbody>
+                </table>
+                i.e. <a href={window.location.origin + window.location.pathname + "?days=" + this.state.days + "&depth=" + this.state.depth + "&startHour=" + this.state.startHour + "&endHour=" + this.state.endHour}>{window.location.origin + window.location.pathname}?days={this.state.days}&depth={this.state.depth}&startHour={this.state.startHour}&endHour={this.state.endHour}</a>
+              </div>
+
             </div>
           </div>
           <p className="info">
